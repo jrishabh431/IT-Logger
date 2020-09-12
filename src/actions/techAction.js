17df9b4ea168be110f8974c24ff7a8b9bab6ac75
Rishabh Jain <rishabhjain94@bitbucket.org>
2020-09-13 04:00:43 +0530

    readme

--- src/actions/techAction.js
+++ src/actions/techAction.js
@@ -1,61 +1,61 @@
 export const setLoading = () => {
   return {
-    type: "SET_LOADING"
+    type: 'SET_LOADING',
   };
 };
 
-export const getTechs = () => async dispatch => {
+export const getTechs = () => async (dispatch) => {
   try {
-    const res = await fetch("/techs");
+    const res = await fetch('/techs');
     const data = await res.json();
 
     dispatch({
-      type: "GET_TECHS",
-      payload: data
+      type: 'GET_TECHS',
+      payload: data,
     });
   } catch (err) {
     dispatch({
-      type: "TECHS_ERROR",
-      payload: err.response.statusText
+      type: 'TECHS_ERROR',
+      payload: err.response.statusText,
     });
   }
 };
 
-export const addTechs = tech => async dispatch => {
+export const addTechs = (tech) => async (dispatch) => {
   try {
-    const res = await fetch("/techs", {
-      method: "POST",
-      body: JSON.stringify(tech),
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(),
       headers: {
-        "Content-Type": "application/json"
-      }
+        'Content-Type': 'application/json',
+      },
     });
     const data = await res.json();
     dispatch({
-      type: "ADD_TECHS",
-      payload: data
+      type: 'ADD_TECHS',
+      payload: data,
     });
   } catch (err) {
     dispatch({
-      type: "TECHS_ERROR",
-      payload: err.response.statusText
+      type: 'TECHS_ERROR',
+      payload: err.response.statusText,
     });
   }
 };
 
-export const deleteTechs = id => async dispatch => {
+export const deleteTechs = (id) => async (dispatch) => {
   try {
     await fetch(`/techs/${id}`, {
-      method: "DELETE"
+      method: 'DELETE',
     });
     dispatch({
-      type: "DELETE_TECHS",
-      payload: id
+      type: 'DELETE_TECHS',
+      payload: id,
     });
   } catch (err) {
     dispatch({
-      type: "TECHS_ERROR",
-      payload: err.response.statusText
+      type: 'TECHS_ERROR',
+      payload: err.response.statusText,
     });
   }
 };
